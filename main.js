c9d40849ebf7fa7e7f451fefd0c4b6f843735b5a
Luka Piskorec <13149424+lukapiskorec@users.noreply.github.com>
2025-11-15 11:05:45 +0200

    Module imports, basic file structure

---
+++ main.js
@@ -0,0 +1,9 @@
+// MODULE IMPORTS
+
+import * as THREE from 'three';
+import { OrbitControls } from 'OrbitControls';
+import { GLTFLoader } from 'GLTFLoader';
+import { EffectComposer } from 'EffectComposer';
+import { RenderPass } from 'RenderPass';
+import { UnrealBloomPass } from 'UnrealBloomPass';
+import { ShaderPass } from 'ShaderPass';
